9a79de083a402d1e4c809f5e2d2f190ff5045291
jcbjcbjc <jcbjcbjc@Arch.localdomain>
2022-11-03 03:01:25 +0800

    update

--- src/base/BoundedBlockingQueue.h
+++ src/base/BoundedBlockingQueue.h
@@ -3,99 +3,88 @@
 //
 // Author: Shuo Chen (chenshuo at chenshuo dot com)
 
-#ifndef common_BASE_BOUNDEDBLOCKINGQUEUE_H
-#define common_BASE_BOUNDEDBLOCKINGQUEUE_H
+#ifndef MUDUO_BASE_BOUNDEDBLOCKINGQUEUE_H
+#define MUDUO_BASE_BOUNDEDBLOCKINGQUEUE_H
 
 #include "Condition.h"
 #include "Mutex.h"
 
-#include <boost/circular_buffer.hpp>
+#include <deque>
 #include <assert.h>
 
 namespace common
 {
 
-template<typename T>
-class BoundedBlockingQueue : noncopyable
-{
- public:
-  explicit BoundedBlockingQueue(int maxSize)
-    : mutex_(),
-      notEmpty_(mutex_),
-      notFull_(mutex_),
-      queue_(maxSize)
-  {
-  }
-
-  void put(const T& x)
-  {
-    MutexLockGuard lock(mutex_);
-    while (queue_.full())
+    template<typename T>
+    class BoundedBlockingQueue : noncopyable
     {
-      notFull_.wait();
-    }
-    assert(!queue_.full());
-    queue_.push_back(x);
-    notEmpty_.notify();
-  }
+    public:
+        explicit BoundedBlockingQueue(unsigned maxSize)
+                : maxSize_(maxSize),
+                  mutex_(),
+                  notEmpty_(mutex_),
+                  notFull_(mutex_)
+        {
+        }
 
-  void put(T&& x)
-  {
-    MutexLockGuard lock(mutex_);
-    while (queue_.full())
-    {
-      notFull_.wait();
-    }
-    assert(!queue_.full());
-    queue_.push_back(std::move(x));
-    notEmpty_.notify();
-  }
+        void put(const T& x)
+        {
+            MutexLockGuard lock(mutex_);
+            while (queue_.size() == maxSize_)
+            {
+                notFull_.wait();
+            }
+            assert(queue_.size() < maxSize_);
+            queue_.push_back(x);
+            notEmpty_.notify();
+        }
 
-  T take()
-  {
-    MutexLockGuard lock(mutex_);
-    while (queue_.empty())
-    {
-      notEmpty_.wait();
-    }
-    assert(!queue_.empty());
-    T front(std::move(queue_.front()));
-    queue_.pop_front();
-    notFull_.notify();
-    return front;
-  }
+        T take()
+        {
+            MutexLockGuard lock(mutex_);
+            while (queue_.empty())
+            {
+                notEmpty_.wait();
+            }
+            assert(!queue_.empty());
+            T front(queue_.front());
+            queue_.pop_front();
+            notFull_.notify();
+            return front;
+        }
 
-  bool empty() const
-  {
-    MutexLockGuard lock(mutex_);
-    return queue_.empty();
-  }
+        bool empty() const
+        {
+            MutexLockGuard lock(mutex_);
+            return queue_.empty();
+        }
 
-  bool full() const
-  {
-    MutexLockGuard lock(mutex_);
-    return queue_.full();
-  }
+        bool full() const
+        {
+            MutexLockGuard lock(mutex_);
+            return queue_.size() == maxSize_;
+        }
 
-  size_t size() const
-  {
-    MutexLockGuard lock(mutex_);
-    return queue_.size();
-  }
+        size_t size() const
+        {
+            MutexLockGuard lock(mutex_);
+            return queue_.size();
+        }
 
-  size_t capacity() const
-  {
-    MutexLockGuard lock(mutex_);
-    return queue_.capacity();
-  }
+        size_t capacity() const
+        {
+            MutexLockGuard lock(mutex_);
+            return queue_.capacity();
+        }
 
- private:
-  mutable MutexLock          mutex_;
-  Condition                  notEmpty_ GUARDED_BY(mutex_);
-  Condition                  notFull_ GUARDED_BY(mutex_);
-  boost::circular_buffer<T>  queue_ GUARDED_BY(mutex_);
-};
+    private:
+        const unsigned             maxSize_;
+        mutable MutexLock          mutex_;
+        Condition                  notEmpty_ GUARDED_BY(mutex_);
+        Condition                  notFull_ GUARDED_BY(mutex_);
+        std::deque<T>              queue_ GUARDED_BY(mutex_);
+    };
 
-}  // namespace common
+}  // namespace muduo
 
-#endif  // common_BASE_BOUNDEDBLOCKINGQUEUE_H
+#endif  // MUDUO_BASE_BOUNDEDBLOCKINGQUEUE_H
--- src/base/Timestamp.h
+++ src/base/Timestamp.h
@@ -3,13 +3,11 @@
 //
 // Author: Shuo Chen (chenshuo at chenshuo dot com)
 
-#ifndef common_BASE_TIMESTAMP_H
-#define common_BASE_TIMESTAMP_H
+#ifndef MUDUO_BASE_TIMESTAMP_H
+#define MUDUO_BASE_TIMESTAMP_H
 
 #include "copyable.h"
 #include "Types.h"
-
-#include <boost/operators.hpp>
 
 namespace common
 {
@@ -20,79 +18,77 @@ namespace common
 /// This class is immutable.
 /// It's recommended to pass it by value, since it's passed in register on x64.
 ///
-class Timestamp : public common::copyable,
-                  public boost::equality_comparable<Timestamp>,
-                  public boost::less_than_comparable<Timestamp>
-{
- public:
-  ///
-  /// Constucts an invalid Timestamp.
-  ///
-  Timestamp()
-    : microSecondsSinceEpoch_(0)
-  {
-  }
+    class Timestamp : public common::copyable
+    {
+    public:
+        ///
+        /// Constucts an invalid Timestamp.
+        ///
+        Timestamp()
+                : microSecondsSinceEpoch_(0)
+        {
+        }
 
-  ///
-  /// Constucts a Timestamp at specific time
-  ///
-  /// @param microSecondsSinceEpoch
-  explicit Timestamp(int64_t microSecondsSinceEpochArg)
-    : microSecondsSinceEpoch_(microSecondsSinceEpochArg)
-  {
-  }
+        ///
+        /// Constucts a Timestamp at specific time
+        ///
+        /// @param microSecondsSinceEpoch
+        explicit Timestamp(int64_t microSecondsSinceEpochArg)
+                : microSecondsSinceEpoch_(microSecondsSinceEpochArg)
+        {
+        }
 
-  void swap(Timestamp& that)
-  {
-    std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_);
-  }
+        void swap(Timestamp& that)
+        {
+            std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_);
+        }
 
-  // default copy/assignment/dtor are Okay
+        // default copy/assignment/dtor are Okay
 
-  string toString() const;
-  string toFormattedString(bool showMicroseconds = true) const;
+        string toString() const;
+        string toFormattedString(bool showMicroseconds = true) const;
 
-  bool valid() const { return microSecondsSinceEpoch_ > 0; }
+        bool valid() const { return microSecondsSinceEpoch_ > 0; }
 
-  // for internal usage.
-  int64_t microSecondsSinceEpoch() const { return microSecondsSinceEpoch_; }
-  time_t secondsSinceEpoch() const
-  { return static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond); }
+        // for internal usage.
+        int64_t microSecondsSinceEpoch() const { return microSecondsSinceEpoch_; }
+        time_t secondsSinceEpoch() const
+        { return static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond); }
 
-  ///
-  /// Get time of now.
-  ///
-  static Timestamp now();
-  static Timestamp invalid()
-  {
-    return Timestamp();
-  }
+        ///
+        /// Get time of now.
+        ///
+        static Timestamp now();
+        static Timestamp invalid()
+        {
+            return Timestamp();
+        }
 
-  static Timestamp fromUnixTime(time_t t)
-  {
-    return fromUnixTime(t, 0);
-  }
+        static Timestamp fromUnixTime(time_t t)
+        {
+            return fromUnixTime(t, 0);
+        }
 
-  static Timestamp fromUnixTime(time_t t, int microseconds)
-  {
-    return Timestamp(static_cast<int64_t>(t) * kMicroSecondsPerSecond + microseconds);
-  }
+        static Timestamp fromUnixTime(time_t t, int microseconds)
+        {
+            return Timestamp(static_cast<int64_t>(t) * kMicroSecondsPerSecond + microseconds);
+        }
 
-  static const int kMicroSecondsPerSecond = 1000 * 1000;
+        static const int kMicroSecondsPerSecond = 1000 * 1000;
 
- private:
-  int64_t microSecondsSinceEpoch_;
-};
+    private:
+        int64_t microSecondsSinceEpoch_;
+    };
 
-inline bool operator<(Timestamp lhs, Timestamp rhs)
-{
-  return lhs.microSecondsSinceEpoch() < rhs.microSecondsSinceEpoch();
-}
+    inline bool operator<(Timestamp lhs, Timestamp rhs)
+    {
+        return lhs.microSecondsSinceEpoch() < rhs.microSecondsSinceEpoch();
+    }
 
-inline bool operator==(Timestamp lhs, Timestamp rhs)
-{
-  return lhs.microSecondsSinceEpoch() == rhs.microSecondsSinceEpoch();
-}
+    inline bool operator==(Timestamp lhs, Timestamp rhs)
+    {
+        return lhs.microSecondsSinceEpoch() == rhs.microSecondsSinceEpoch();
+    }
 
 ///
 /// Gets time difference of two timestamps, result in seconds.
@@ -101,23 +97,23 @@ inline bool operator==(Timestamp lhs, Timestamp rhs)
 /// @return (high-low) in seconds
 /// @c double has 52-bit precision, enough for one-microsecond
 /// resolution for next 100 years.
-inline double timeDifference(Timestamp high, Timestamp low)
-{
-  int64_t diff = high.microSecondsSinceEpoch() - low.microSecondsSinceEpoch();
-  return static_cast<double>(diff) / Timestamp::kMicroSecondsPerSecond;
-}
+    inline double timeDifference(Timestamp high, Timestamp low)
+    {
+        int64_t diff = high.microSecondsSinceEpoch() - low.microSecondsSinceEpoch();
+        return static_cast<double>(diff) / Timestamp::kMicroSecondsPerSecond;
+    }
 
 ///
 /// Add @c seconds to given timestamp.
 ///
 /// @return timestamp+seconds as Timestamp
 ///
-inline Timestamp addTime(Timestamp timestamp, double seconds)
-{
-  int64_t delta = static_cast<int64_t>(seconds * Timestamp::kMicroSecondsPerSecond);
-  return Timestamp(timestamp.microSecondsSinceEpoch() + delta);
-}
+    inline Timestamp addTime(Timestamp timestamp, double seconds)
+    {
+        int64_t delta = static_cast<int64_t>(seconds * Timestamp::kMicroSecondsPerSecond);
+        return Timestamp(timestamp.microSecondsSinceEpoch() + delta);
+    }
 
-}  // namespace common
+}  // namespace muduo
 
-#endif  // common_BASE_TIMESTAMP_H
+#endif  // MUDUO_BASE_TIMESTAMP_H
